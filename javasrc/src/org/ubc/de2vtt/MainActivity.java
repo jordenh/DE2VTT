e88a711c457a8a8c992c3c3cf06b16c95fd7a145
Gord Larson <gordlarson@gmail.com>
2013-10-22 18:57:31 -0700

    Add basic manager or shared preferences

--- javasrc/src/org/ubc/de2vtt/MainActivity.java
+++ javasrc/src/org/ubc/de2vtt/MainActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
@@ -17,11 +17,14 @@ public class MainActivity extends Activity {
 	private String[] mDrawerItems;
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
+	private static Context mContext;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mContext = getApplicationContext();
         
         mDrawerItems = getResources().getStringArray(R.array.app_drawer_array);
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
@@ -32,7 +35,6 @@ public class MainActivity extends Activity {
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -71,5 +73,13 @@ public class MainActivity extends Activity {
     @Override
     public void setTitle(CharSequence title) {
     	getActionBar().setTitle(title);
+    }
+    
+    /**
+     * Provides a static way to get the application context
+     * @return application context
+     */
+    static public Context getAppContext() {
+    	return mContext;
     }
 }
---
+++ javasrc/src/org/ubc/de2vtt/SharedPreferencesManager.java
@@ -0,0 +1,48 @@
+package org.ubc.de2vtt;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+public class SharedPreferencesManager {
+	private final String TAG = SharedPreferencesManager.class.getSimpleName();
+	
+	private static final String SHARED_PREFS_NAME = "DE2VTTPrefs";
+	
+	private static SharedPreferencesManager mSharedInstance;
+	
+	private SharedPreferences mSharedPrefs;
+	private Editor mEditor;
+	
+	/**
+	 * Gets the shared preferences manager singleton
+	 * @return Shared instance of the managers
+	 */
+	public static SharedPreferencesManager getSharedInstance() {
+		if (mSharedInstance == null) {
+			mSharedInstance = new SharedPreferencesManager();
+		}
+		return mSharedInstance;
+	}
+	
+	protected SharedPreferencesManager() {
+		Context context = MainActivity.getAppContext();
+		mSharedPrefs = context.getSharedPreferences(SHARED_PREFS_NAME, 
+				Context.MODE_PRIVATE);
+		mEditor = mSharedPrefs.edit();
+	}
+	
+	// TODO: Add putXXX / getXXX methods as needed
+
+	public boolean putInt(String key, int val) {
+		mEditor.putInt(key, val);
+		return mEditor.commit();
+	}
+	
+	/**
+	 * @return value in shared prefs matching key, otherwise defaultValue
+	 */
+	public int getInt(String key, int defaultValue) {
+		return mSharedPrefs.getInt(key, defaultValue);
+	}
+}
