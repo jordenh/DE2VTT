42e4fa877ed7bde082adc9447b8b681d2f93dcd7
Jorden Hetherington <jorden.hetherington@gmail.com>
2013-10-25 18:08:28 -0700

    creating basic c code basis

---
+++ csrc/obj/default/hello_world.d
@@ -0,0 +1,25 @@
+obj/default/hello_world.o: hello_world.c \
+  C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//drivers/inc/altera_up_avalon_rs232.h \
+  C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//HAL/inc/alt_types.h \
+  C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//HAL/inc/sys/alt_dev.h \
+  C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW/system.h \
+  C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW/linker.h \
+  C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//HAL/inc/sys/alt_llist.h \
+  C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//HAL/inc/priv/alt_dev_llist.h \
+  C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//HAL/inc/sys/alt_llist.h
+
+C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//drivers/inc/altera_up_avalon_rs232.h:
+
+C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//HAL/inc/alt_types.h:
+
+C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//HAL/inc/sys/alt_dev.h:
+
+C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW/system.h:
+
+C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW/linker.h:
+
+C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//HAL/inc/sys/alt_llist.h:
+
+C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//HAL/inc/priv/alt_dev_llist.h:
+
+C:/Users/jorde_000/Documents/GitHub/DE2VTT_HW//HAL/inc/sys/alt_llist.h:
