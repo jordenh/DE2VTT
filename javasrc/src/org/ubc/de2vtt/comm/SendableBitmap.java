014ae385f6476090d26c53b3a76d0a9779005772
Gord Larson <gordlarson@gmail.com>
2013-10-31 18:12:49 -0700

    add first draft of sendablebitmap

---
+++ javasrc/src/org/ubc/de2vtt/comm/SendableBitmap.java
@@ -0,0 +1,23 @@
+package org.ubc.de2vtt.comm;
+
+import java.nio.ByteBuffer;
+
+import android.graphics.*;
+
+public class SendableBitmap implements Sendable {
+	private Bitmap bmp;
+	
+	public SendableBitmap(Bitmap bmp) {
+		this.bmp = bmp;
+	}
+	
+	@Override
+	public byte[] ToByteArray() {
+		int size = bmp.getRowBytes() * bmp.getHeight();
+		ByteBuffer b = ByteBuffer.allocate(size);
+
+		bmp.copyPixelsToBuffer(b);
+		
+		return b.array();
+	}
+}
