b4857154ae25a84a96162a1cceeb2259124e08ac
Jorden Hetherington <jorden.hetherington@gmail.com>
2013-11-05 18:08:49 -0800

    adding command files

---
+++ csrc/command.h
@@ -0,0 +1,16 @@
+
+#ifndef COMMAND_H_
+#define COMMAND_H_
+
+/*enum Command {
+	CONNECT((byte)0),
+	DISCONNECT((byte)1),
+	SEND_MAP((byte)2),
+	SEND_TOKEN((byte)3),
+	GET_DM((byte)4),
+	RELEASE_DM((byte)5),
+	MOVE_TOKEN((byte)6),
+	HANDSHAKE((byte)7); */
+
+
+#endif /* COMMAND_H_ */
