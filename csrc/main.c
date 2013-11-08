5d28532abc4ab51e89e94166a013d7df7efe3f52
Jorden Hetherington <jorden.hetherington@gmail.com>
2013-11-07 16:27:52 -0800

    building command code

--- csrc/main.c
+++ csrc/main.c
@@ -1,82 +1,3 @@
-/*#include <stdio.h>
-#include <stdlib.h>
-#include<string.h>
-#include "audio.h"
-#include "timer.h"
-#include "sd_card.h"
-#include "vga.h"
-#include "bmp.h"
-#include "input.h"
-#include "io.h"
-#include "system.h"
-#include "altera_nios2_qsys_irq.h"
-#include "sys/alt_irq.h"
-
-int init(void) {
-	if (openSdCard() == -1) {
-		printf("Error: Failed to open sd card\n");
-		return -1;
-	} else {
-		printf("Opened SD card\n");
-	}
-
-	initVga();
-	//parseBmps();
-	//setupAudio();
-
-	initHardwareTimer();
-
-	return 0;
-}
-
-int main() {
-	int length;
-	char* msg = "0Detected the character 't'.\n";
-	FILE* fp;
-	char cmd[128];
-
-	fp = fopen ("/dev/uart_0", "r+"); //Open file for reading and writing
-
-	if (fp)
-	{
-		fputc(strlen(msg), fp);
-		fwrite (msg, strlen(msg), 1, fp);
-
-		while(1) {
-			length = (int) fgetc(fp);
-			fread(cmd, length, 1, fp);
-			printf("%s", cmd);
-		}
-
-		fclose (fp);
-	}
-	//***********************************
-
-	return;
-	if (init() == -1)
-		return -1;
-
-	startHardwareTimer();
-
-	// main game loop;
-	while (1) {
-		if (hasHardwareTimerExpired() == 1) {
-			startHardwareTimer();
-
-			handleKeyInput();
-			handleSwitchInput();
-
-
-			playEpicMusic();
-		}
-	}
-
-	return 0;
-}
-
-*/
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "audio.h"
@@ -112,57 +33,8 @@ int init(void) {
 }
 
 int main() {
-	//********* new ******************************************
-	/*
-	int i;
-	unsigned char data;
-	unsigned char parity;
-	unsigned char message[] = "EECE381 is so much fun";
-
-	printf("UART Initialization\n");
-	alt_up_rs232_dev* uart = alt_up_rs232_open_dev(RS232_0_NAME);
-
-	printf("Clearing read buffer to start\n");
-	while (alt_up_rs232_get_used_space_in_read_FIFO(uart)) {
-		alt_up_rs232_read_data(uart, &data, &parity);
-	}
-
-	printf("Sending the message to the Middleman\n");
-
-	// Start with the number of bytes in our message
-	alt_up_rs232_write_data(uart, (unsigned char) strlen(message));
-
-	// Now send the actual message to the Middleman
-
-	for (i = 0; i < strlen(message); i++) {
-		alt_up_rs232_write_data(uart, message[i]);
-	}
-
-	// Now receive the message from the Middleman
-
-	printf("Waiting for data to come back from the Middleman\n");
-	while (alt_up_rs232_get_used_space_in_read_FIFO(uart) == 0)
-		;
-
-	// First byte is the number of characters in our message
-
-	alt_up_rs232_read_data(uart, &data, &parity);
-	int num_to_receive = (int) data;
-
-	printf("About to receive %d characters:\n", num_to_receive);
-
-	for (i = 0; i < num_to_receive; i++) {
-		while (alt_up_rs232_get_used_space_in_read_FIFO(uart) == 0)
-			;
-		alt_up_rs232_read_data(uart, &data, &parity);
-
-		printf("%c", data);
-	}
-	printf("\n");
-	printf("Message Echo Complete\n");
-
-	// ***********************************
-	*/
+	message msg;
+	int statusInt;
 
 	if (init() == -1)
 		return -1;
@@ -171,19 +43,27 @@ int main() {
 
 	// main game loop;
 	while (1) {
-		if (hasHardwareTimerExpired() == 1) {
+		//receive msg
+		//execute command
+		if(msg.buffer != NULL) {
+			free(msg.buffer);
+		}
+		msg = getMessage();
+		statusInt = executeCmd(msg.cmd);
+
+		if(statusInt == -1) {
+			printf("error occured in executing Command.\n");
+		}
+
+
+		/*if (hasHardwareTimerExpired() == 1) {
 			startHardwareTimer();
 
 			handleKeyInput();
 			handleSwitchInput();
-
 			//playEpicMusic();
-
 			//Check if message to receive?
-				//receive msg
-				//execute command
-
-		}
+		}*/
 	}
 
 	freeBmps();
