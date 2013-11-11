065e178c73f19902d820b1be32d49f794fc4a851
Gord Larson <gordlarson@gmail.com>
2013-11-11 11:58:25 -0800

    modify send bitmap

--- javasrc/src/org/ubc/de2vtt/comm/sendables/SendableBitmap.java
+++ javasrc/src/org/ubc/de2vtt/comm/sendables/SendableBitmap.java
@@ -16,11 +16,28 @@ public class SendableBitmap implements Sendable {
 	
 	@Override
 	public byte[] ToByteArray() {
+		int width = bmp.getWidth();
+		int height = bmp.getHeight();
+		
+		byte widthBuf[] = ByteBuffer.allocate(4).putInt(width).array();
+		byte heightBuf[] = ByteBuffer.allocate(4).putInt(height).array();
+		
 		int size = bmp.getRowBytes() * bmp.getHeight();
 		ByteBuffer b = ByteBuffer.allocate(size);
-
 		bmp.copyPixelsToBuffer(b);
+		byte pixelBuf[] = b.array();
 		
-		return b.array();
+		byte ret[] = new byte[widthBuf.length + heightBuf.length + pixelBuf.length];
+		int cursor = 0;
+		putInt(widthBuf, ret, cursor);
+		putInt(heightBuf, ret, cursor);
+		System.arraycopy(pixelBuf, 0, ret, cursor, pixelBuf.length);
+		
+		return ret;
+	}
+
+	private void putInt(byte[] intBuf, byte[] dest, Integer cursor) {
+		System.arraycopy(intBuf, 0, dest, cursor, intBuf.length);
+		cursor += intBuf.length;
 	}
 }
