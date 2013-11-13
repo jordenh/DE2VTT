f5aeab469581573f6df94dff7c04c07b179b1a5a
Jorden Hetherington <jorden.hetherington@gmail.com>
2013-11-12 16:16:48 -0800

    fixed RGB on android side

--- javasrc/src/org/ubc/de2vtt/comm/sendables/SendableBitmap.java
+++ javasrc/src/org/ubc/de2vtt/comm/sendables/SendableBitmap.java
@@ -27,6 +27,22 @@ public class SendableBitmap implements Sendable {
 
 		bmp.copyPixelsToBuffer(b);
 		byte pixelBuf[] = b.array();
+		byte sendPixBuf[] = new byte[pixelBuf.length / 2]; // use half the bytes in transmission.
+		
+		int j;
+		byte red,green,blue;
+		//TBD - make pixelBuff 2 byte arr.
+		for(int i = 0; i < pixelBuf.length; i+=4) {
+			j = i / 2;
+			red = (byte) ((pixelBuf[i] & 0xF1) >> 3);
+			green = (byte) ((pixelBuf[i+1] & 0xFC) >> 2);
+			blue = (byte) ((pixelBuf[i+2] & 0xF1) >> 3);
+			
+			int d = (red << 11) | (green << 5) | blue;
+			
+			sendPixBuf[j] = (byte) (d >> 8);
+			sendPixBuf[j + 1] = (byte) d;
+		}
 		
 		byte ret[] = new byte[widthBuf.length + heightBuf.length + pixelBuf.length];
 		//Integer cursor = new Integer(0);
