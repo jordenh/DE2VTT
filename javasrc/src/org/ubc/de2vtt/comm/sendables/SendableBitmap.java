3a385e9cb8225c5717416fc20ab750de945f38a4
Gord Larson <gordlarson@gmail.com>
2013-11-12 16:36:49 -0800

    stuff

--- javasrc/src/org/ubc/de2vtt/comm/sendables/SendableBitmap.java
+++ javasrc/src/org/ubc/de2vtt/comm/sendables/SendableBitmap.java
@@ -32,11 +32,11 @@ public class SendableBitmap implements Sendable {
 		int j;
 		byte red,green,blue;
 		//TBD - make pixelBuff 2 byte arr.
-		for(int i = 0; i < pixelBuf.length; i+=4) {
+		for(int i = 0; i < pixelBuf.length; i += 4) {
 			j = i / 2;
 			red = (byte) ((pixelBuf[i] & 0xF1) >> 3);
-			green = (byte) ((pixelBuf[i+1] & 0xFC) >> 2);
-			blue = (byte) ((pixelBuf[i+2] & 0xF1) >> 3);
+			green = (byte) ((pixelBuf[i + 1] & 0xFC) >> 2);
+			blue = (byte) ((pixelBuf[i + 2] & 0xF1) >> 3);
 			
 			int d = (red << 11) | (green << 5) | blue;
 			
@@ -44,13 +44,13 @@ public class SendableBitmap implements Sendable {
 			sendPixBuf[j + 1] = (byte) d;
 		}
 		
-		byte ret[] = new byte[widthBuf.length + heightBuf.length + pixelBuf.length];
+		byte ret[] = new byte[widthBuf.length + heightBuf.length + sendPixBuf.length];
 		//Integer cursor = new Integer(0);
 		//putInt(widthBuf, ret, cursor);
 		//putInt(heightBuf, ret, cursor);
 		System.arraycopy(widthBuf, 0, ret, 0, widthBuf.length);
 		System.arraycopy(heightBuf, 0, ret, 4, heightBuf.length);
-		System.arraycopy(pixelBuf, 0, ret, 8, pixelBuf.length);
+		System.arraycopy(sendPixBuf, 0, ret, 8, sendPixBuf.length);
 		
 		return ret;
 	}
