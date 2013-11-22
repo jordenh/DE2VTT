dec85bd3882b566f0308a7aecebe31ba650c332a
Jorden Hetherington <jorden.hetherington@gmail.com>
2013-11-21 23:56:35 -0800

    making notifications - currently works, but trying to add an intent to open fragment

---
+++ javasrc/src/org/ubc/de2vtt/notifications/notifications.java
@@ -0,0 +1,67 @@
+package org.ubc.de2vtt.notifications;
+
+import org.ubc.de2vtt.MainActivity;
+import org.ubc.de2vtt.R;
+import org.ubc.de2vtt.fragments.BulletinFragment;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+
+
+public class notifications {
+		
+	public static void notifyOfNewMessage() {
+		Context context = MainActivity.getAppContext();
+		int mId = 1;
+		NotificationCompat.Builder mBuilder =
+				new NotificationCompat.Builder(MainActivity.getAppContext())
+		        .setSmallIcon(R.drawable.ic_launcher)
+		        .setContentTitle("New message in WING.")
+		        .setContentText("Pending message from __XX__!");
+
+		
+		
+		// Creates an explicit intent for an Activity in your app
+		Intent resultIntent = new Intent(context, BulletinFragment.class);
+
+		// The stack builder object will contain an artificial back stack for the
+		// started Activity.
+		// This ensures that navigating backward from the Activity leads out of
+		// your application to the Home screen.
+//		TaskStackBuilder stackBuilder = TaskStackBuilder.create(MainActivity.getAppContext());
+		// Adds the back stack for the Intent (but not the Intent itself)
+//		stackBuilder.addParentStack(BulletinFragment.class);
+		// Adds the Intent that starts the Activity to the top of the stack
+/*		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent =
+		        stackBuilder.getPendingIntent(
+		            0,
+		            PendingIntent.FLAG_UPDATE_CURRENT
+		        ); */
+		PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+		mBuilder.setContentIntent(resultPendingIntent);
+		NotificationManager mNotificationManager = 
+				(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE); 
+		// mId allows you to update the notification later on. 
+		mNotificationManager.notify(mId, mBuilder.build());
+	}
+
+}
+
+/* old code dump:
+ * 
+ * 		
+		//Temp to try to issue a basic notification:
+		NotificationManager mNotificationManager =
+		    (NotificationManager) getNotificationManager();
+		NotificationManager.notify(0, mBuilder);
+		// mId allows you to update the notification later on.
+		//mNotificationManager.notify(mId, mBuilder.build());
+
+
+
+*/
